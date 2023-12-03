83c3c2ca4282593da23a6a12a6c27371d7b83cdb
Arman-Kabir <armankabir8@gmail.com>
2023-12-03 15:41:33 +0600

    few base styles added

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,19 @@
+
+import './Hero.css';
+
+const HeroSection = () => {
+  return (
+    <section className="hero" id="hero">
+        <div className="container hero-container">
+            <div className="hero-content">
+                <h3>Hi, I'm</h3>
+                <h2>Arman Kabir</h2>
+                <h1>Full-Stack <br /> Web Developer</h1>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default HeroSection
