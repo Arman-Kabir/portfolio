4b38f77ac1d58ea9807637af8e58698f90c64571
Arman-Kabir <armankabir8@gmail.com>
2023-12-23 12:31:56 +0600

    design changed and hero added

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,21 @@
-
 import './Hero.css';
+import hero_image from '../../assets/images/arman2.png';
 
 const HeroSection = () => {
   return (
     <section className="hero" id="hero">
-        <div className="container hero-container">
-            <div className="hero-content">
-                <h3>Hi, I'm</h3>
-                <h2>Arman Kabir</h2>
-                <h1>Full-Stack <br /> Web Developer</h1>
-            </div>
+      <div className="container hero-container">
+        <div className="hero-content">
+          <div>
+            <h3>Hi, I'm</h3>
+            <h2>Arman Kabir</h2>
+            <h1>Full-Stack <br /> Web Developer</h1>
+          </div>
         </div>
+        <div className='hero_image'>
+          <img src={hero_image} alt="Arman's Img" className='rounded__image' />
+        </div>
+      </div>
 
     </section>
   )
