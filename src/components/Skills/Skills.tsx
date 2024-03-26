e4e2c218e96d0f18a08f487192ccc2d20d263e8b
Arman-Kabir <armankabir8@gmail.com>
2024-03-26 14:07:52 +0600

    backend skills added

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,9 +4,10 @@ import './Skills.css';
 const Skills = () => {
     return (
         <section className='section_container'>
+
             <SectionHeader title={'Skills I Have'} />
 
-            <div className='container'>
+            <div className='container skills_container'>
 
                 <div className='front_end'>
 
@@ -21,13 +22,13 @@ const Skills = () => {
                             <p>NextJs</p>
                             <p>Redux-Toolkit</p>
                             <p>GraphQL</p>
+                            <p>React-Native</p>
                         </div>
                         <div className='second_row'>
                             <p>React Query</p>
                             <p>RTK Query</p>
                             <p>React Router</p>
                             <p>Axios</p>
-                            <p>React-Native</p>
                         </div>
                         <div className='third_row'>
                             <p>HTML/CSS</p>
@@ -46,7 +47,20 @@ const Skills = () => {
                     </div>
 
                     <div className='stack_details'>
+                        <div className='first_row'>
+                            <p>Node.js</p>
+                            <p>Express.js</p>
+                            <p>MongoDB</p>
+                            <p>Mongoose</p>
+                            <p>Prisma</p>
+                        </div>
+                        <div className='second_row'>
+                            <p>SQL</p>
+                            <p>PostGreSQL</p>
+                        </div>
+                        <div className='third_row'>
 
+                        </div>
                     </div>
                 </div>
 
