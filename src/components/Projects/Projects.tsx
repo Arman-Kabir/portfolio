d63ed9ea9afc7e62fb281a2e735fdd1ee6e32127
Arman-Kabir <armankabir8@gmail.com>
2024-04-16 19:21:11 +0600

    few data added

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,16 +6,22 @@ const Projects = () => {
         console.log(arg);
     }
 
-    
+
     return (
         <section className='section_container'>
             <SectionHeader title="My Projects"></SectionHeader>
             <div className="  container projects_container">
+                
                 <div className='project_categories'>
+
                     <button onClick={() => handleButton('all')} className='btn btn_dark'>All</button>
+
                     <button onClick={() => handleButton('react')} className='btn btn_dark'>ReactJs</button>
+
                     <button onClick={() => handleButton('next')} className='btn btn_dark'>NextJs</button>
+
                     <button onClick={() => handleButton('node')} className='btn btn_dark'>Node</button>
+
                     <button onClick={() => handleButton('landing')} className='btn btn_dark'>Landing Page</button>
 
                 </div>
