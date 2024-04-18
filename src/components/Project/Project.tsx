80f06a4ff8831c34f238b0337071c3466b1e6783
Arman-Kabir <armankabir8@gmail.com>
2024-04-18 16:22:44 +0600

    data fetched

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,9 @@
+import './Project.css';
+
+
+export default function Project({project}) {
+    console.log(project);
+  return (
+    <div>Project</div>
+  )
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from 'react';
 import { SectionHeader } from '../SectionHeader/SectionHeader';
 import './Projects.css';
+import Project from '../Project/Project';
 
 const Projects = () => {
+    const [data, setData] = useState([]);
+
+    console.log(data);
     function handleButton(arg) {
         console.log(arg);
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('data.json');
+                const jsonData = await response.json();
+                setData(jsonData);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, [])
 
 
     return (
         <section className='section_container'>
             <SectionHeader title="My Projects"></SectionHeader>
             <div className="  container projects_container">
-                
+
                 <div className='project_categories'>
 
                     <button onClick={() => handleButton('all')} className='btn btn_dark'>All</button>
@@ -24,6 +44,14 @@ const Projects = () => {
 
                     <button onClick={() => handleButton('landing')} className='btn btn_dark'>Landing Page</button>
 
+                </div>
+
+                <div className="project_lists">
+                    {data &&
+                        data.map((project) => (
+                            <Project project={project} key={project.id}></Project>
+                        ))
+                    }
                 </div>
 
             </div>
